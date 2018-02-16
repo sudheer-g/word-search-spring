2534d64511d80865ebf26e6fb9abef7b62440722
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-02-16 18:39:28 +0530

    Returning JSON as result automatically with spring and jackson converter

--- src/main/java/com/controllers/WordSearchController.java
+++ src/main/java/com/controllers/WordSearchController.java
@@ -6,18 +6,16 @@ import com.work.assignments.FileIO.Query;
 import com.work.assignments.FileIO.Result;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequestMapping(value = "/")
 public class WordSearchController {
 
@@ -26,28 +24,16 @@ public class WordSearchController {
         return new ModelAndView("wordSearch", "word", new Word());
     }
 
-    @PostMapping(value = "/results")
-    public @ResponseBody
-    String searchResults(@ModelAttribute("word") Word word, BindingResult bindingResult) {
+    @PostMapping(value = "/results", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public List<Result> searchResults(@ModelAttribute("word") Word word, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "error";
+            return null;
         }
         if (!word.getWord().equals("") && !word.getDirectory().equals("")) {
             System.out.println("WordSearch Hit: Get Word: " + word.getWord() + "Get Directory: " + word.getDirectory());
             MultiThreadedWordSearchService wordSearchService = new MultiThreadedWordSearchService();
-            List<Result> resultList = wordSearchService.search(new Query(word.getDirectory(), word.getWord(), true));
-            JSONArray resultArray = new JSONArray();
-            for (Result result : resultList) {
-                JSONObject obj = new JSONObject();
-                obj.put("lineNumber", result.getLineNumber());
-                obj.put("positionNumber", result.getPositionNumber());
-                obj.put("fileName", result.getFileName());
-                resultArray.add(obj);
-            }
-            return resultArray.toJSONString();
+            return wordSearchService.search(new Query(word.getDirectory(), word.getWord(), true));
         }
         return null;
     }
-
-
 }
